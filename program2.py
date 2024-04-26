e4d6ce81e638264f9410af3a1e8d4f5760616b5d
khwahish_001 <khwahish.522524@gmail.com>
2024-04-26 12:43:04 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,14 +1,3 @@
-def longest_substring(s: str) -> int:
-    
-    """"
-    
-    Implement the function longest_substring 
-    using the provided longest_substring function to find 
-    the length of the longest substring without repeating characters.
-
-    """ 
-    pass
-
 
 
 def longest_substring(s: str) -> int:
