56e6756b059d8a7115bca8bcabb150f98e285edf
khwahish_001 <khwahish.522524@gmail.com>
2024-04-26 12:47:05 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -8,7 +8,7 @@ def longest_substring(s: str) -> int:
     for end in range(len(s)):
         
         if s[end] in char_index and char_index[s[end]] >= start:
-            # Update the start of the substring to the next character after the last occurrence of the current character
+            
             start = char_index[s[end]] + 1
         # Update the index of the current character
         char_index[s[end]] = end
